45cd7af2376ff480d889cfa0acdfe50994016f89
Juuso Kosonen <juuson@gmail.com>
2021-03-04 19:30:41 +0200

    Add files via upload

---
+++ docs/precache-manifest.e64ef3f5d9df86d68fee59abda21eb70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc44354054ab1908c309c4d91da490e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d11f6bbc8827ccef33",
+    "url": "/static/css/main.80e5fba8.chunk.css"
+  },
+  {
+    "revision": "67acb4c2c4a2a7a679dc",
+    "url": "/static/js/2.3d4288aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d4288aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d11f6bbc8827ccef33",
+    "url": "/static/js/main.58bfce2b.chunk.js"
+  },
+  {
+    "revision": "746cb1a5b05c7d433220",
+    "url": "/static/js/runtime-main.f589ac9b.js"
+  },
+  {
+    "revision": "95acf5ec105d38b81cc386f10e66f10b",
+    "url": "/static/media/bg.95acf5ec.jpg"
+  }
+]);
